95feb56f3a3070cefca69f35ee834d79a9504ac2
Jason Croos <jasoncroos@gmail.com>
2017-08-18 20:49:19 +0530

    Fix
    Fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+fetch('https://jasonthecoder.github.io/db.json')
 .then(response => response.json())
 .then(data => {
   console.log(data);
   for (var i = 0; i < data.length; i++) {
 
    var node = document.createElement("P");
-   var textnode = document.createTextNode(data[i].name);
+   var textnode = document.createTextNode(data[i].key + '-' + data[i].medi);
    node.appendChild(textnode);
    document.getElementById("x").appendChild(node);
   }
